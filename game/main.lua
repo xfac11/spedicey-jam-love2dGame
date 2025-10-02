b95a9850f422229453e4a41251c046b74d5d98da
xfac11 <26203349+xfac11@users.noreply.github.com>
2025-10-02 23:13:24 +0200

    Added player and enemy image and added an svg with enemy and player inside.

--- game/main.lua
+++ game/main.lua
@@ -1,20 +1,31 @@
 https = nil
 local overlayStats = require("lib.overlayStats")
 local runtimeLoader = require("runtime.loader")
+local player = {}
+player.imagePath = "assets/player.png"
+player.image = nil
+player.position = {x = 0, y = 0}
 
 function love.load()
   https = runtimeLoader.loadHTTPS()
   -- Your game load here
+  player.image = love.graphics.newImage(player.imagePath)
+
+
   overlayStats.load() -- Should always be called last
 end
 
 function love.draw()
   -- Your game draw here
+  love.graphics.draw(player.image, player.position.x, player.position.y);
+
   overlayStats.draw() -- Should always be called last
 end
 
 function love.update(dt)
   -- Your game update here
+
+
   overlayStats.update(dt) -- Should always be called last
 end
 
